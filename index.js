65e4b73ed54a9dd618fe1ba2099732caa1120f89
Md.Samsul Arefin <samsularefin532@gmail.com>
2023-12-22 17:21:44 +0600

    server has been set up

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 const express = require('express');
 const cors = require('cors')
 const port = process.env.PORT || 3000;
+require('dotenv').config()
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.wtx9jbs.mongodb.net/?retryWrites=true&w=majority`;
 app.use(cors())
 app.use(express.json())
-
 app.get('/', (req, res) => {
     res.send('server is running..')
 })
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+const DBConnect = () => {
+    try {
+        client.connect();
+        console.log('Task management Database Connected Successfully');
+    }
+    catch (error) {
+        console.log(error.name, error.message);
+    }
+}
+DBConnect();
+
+
+const taskUsers = client.db('Task').collection('taskUsers');
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log('server in running on ', port);
